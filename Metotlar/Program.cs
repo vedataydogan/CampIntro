94572e6739ad164ed07e2fef6e943d1fa77d5ab9
Vedat Aydoğan <vedataydogan07@gmail.com>
2021-01-24 15:06:32 +0300

    16 Ocak Projesi

---
+++ Metotlar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Product[] urunler = new Product[] { urun1, urun2 };
+            
+            foreach (Product urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("--------------------------");
+            }
+
+
+            Console.WriteLine("--------------Metotlar------------");
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void Ekle(Product urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : " + urun.Adi);
+        }
+    }
+}
