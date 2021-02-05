9eaf364a18d137cd1ca20c03e71da000baed8549
Vedat Aydoğan <vedataydogan07@gmail.com>
2021-02-05 15:51:44 +0300

    Sql and GameProject added

---
+++ GameProject/IGamerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    //Manager class ların interface lerini Service diye yazıyoruz. Manager olarak da yazılabilinir.
+    interface IGamerService
+    {
+        //Daha gelişmiş sistemlerde Gamer class ını User olarak yazabiliriz.
+        void Add(Gamer gamer);
+        void Update(Gamer gamer);
+        void Delete(Gamer gamer);
+    }
+}
